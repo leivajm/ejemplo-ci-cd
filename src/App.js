1fbf02801a672587b1acfe3b839d1cd173bb1a5c
José Manuel Leiva <leiva.ruano@gmail.com>
2024-10-02 20:17:51 -0600

    Update app.js with error

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender React
         </a>
       <ButtonComponent></ButtonComponent>
       </header>
